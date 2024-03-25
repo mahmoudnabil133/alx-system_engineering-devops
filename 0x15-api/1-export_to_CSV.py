7ae5521d397759410c3c644a4da9521653cdd6d9
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-26 01:43:31 +0200

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module that get info about todos of some user
+by its id , using an jsonplace hosler api
+"""
+
+import requests
+import sys
+import csv
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user_id)
+    url2 = 'https://jsonplaceholder.typicode.com/users?id={}'.format(user_id)
+    response = requests.get(url)
+    response2 = requests.get(url2)
+    todo_data = response.json()
+    user_data = response2.json()
+    name = user_data[0].get('name')
+    user_todos = []
+    for td in todo_data:
+        td_data = [user_id, name]
+        td_data.append(td.get('completed'))
+        td_data.append(td.get('title'))
+        user_todos.append(td_data)
+
+    csv_file = '{}.csv'.format(user_id)
+    with open(csv_file, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(user_todos)
