c16967d1ee8d468de38541d1549e6fa975914ec8
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-26 00:57:04 +0200

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,29 @@
-#!/usr/bin/env python3
-"apis"
+#!/usr/bin/python3
+"""
+module that get info about todos of some user
+by its id , using an jsonplace hosler api
+"""
+
 import requests
-import json
+import sys
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users/'
-    res = requests.get(url)
-    data = res.json()
-    # name = data.get('name')
-    with open('data.json', 'w') as f:
-        json.dump(data, f, indent=4)
-    
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user_id)
+    url2 = 'https://jsonplaceholder.typicode.com/users?id={}'.format(user_id)
+    response = requests.get(url)
+    response2 = requests.get(url2)
+    todo_data = response.json()
+    user_data = response2.json()
+    name = user_data[0].get('name')
+    comp = 0
+    tot = len(todo_data)
+    for td in todo_data:
+        if td.get('completed'):
+            comp += 1
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name, comp, tot
+    ))
+    for td in todo_data:
+        print('\t {}'.format(td.get('title')))
+    # print(todo_data)
